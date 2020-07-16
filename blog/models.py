18248e2e3a504a6f2edbfc115e087e99e8398c22
testname <testmail@example.com>
2020-07-16 14:12:12 +0900

    0716 commit

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -5,11 +6,16 @@ from django.utils import timezone
 
 class Post(models.Model):
 	#各プロパティにmodels関数が用意している型を定義する（型で初期化ってことかな）
-	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+	#他のモデルへのリンク
+	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	#２００文字までのテキストフィールド
 	title = models.CharField(max_length=200)
+	#制限なしのテキストフィールド
 	text = models.TextField()
+	#日付と時間のフィールド
 	created_date = models.DateTimeField(
 			default=timezone.now)
+	#日付と時間のフィールド
 	published_date = models.DateTimeField(
 			blank=True, null=True)
 			
@@ -21,5 +27,3 @@ class Post(models.Model):
 	
 	def __str__(self):
 		return self.title
-
-
